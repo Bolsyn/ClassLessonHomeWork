0a53739f3469b58385b2c04cb729bf9709c3c0d0
Bolsyn <Serikagis@yandex.ru>
2020-12-19 11:05:53 +0600

    Add project files.

---
+++ ClassLessonHomeWork/Airplane.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLessonHomeWork
+{
+    // 1.Разработать один из классов, в соответствии с полученным вариантом.(Самолет)   
+    public partial class Airplane
+    {   
+        // 2.Реализовать не менее пяти закрытых полей(различных типов), представляющих основные характеристики рассматриваемого класса.
+        private string nameOfCompany;
+        private string codeOfPlane;
+        private int countOfPassangers;
+        private int countOfCrew;
+        private double distanceOfFlight;
+        private int countOfEngines;
+        private double volumeOfFuel;
+
+        // 5.Создать не менее двух статических полей(различных типов),  представляющих общие характеристики объектов данного класса.        
+        static private int countOfCrashes;
+        static private int countOfPlanes;
+
+        // 6.Обязательным требованием является реализация нескольких перегруженных конструкторов, аргументы которых определяются студентом,
+        // исходя из специфики реализуемого класса, а так же реализация конструктора по умолчанию.
+        public Airplane(int countOfPassangers, int countOfCrew, double distanceOfFlight)
+        {
+            this.countOfPassangers = countOfPassangers;
+            this.countOfCrew = countOfCrew;
+            this.distanceOfFlight = distanceOfFlight;
+        }
+
+        public Airplane(int countOfPassangers, int countOfCrew, string codeOfPlane)
+        {
+            this.countOfPassangers = countOfPassangers;
+            this.countOfCrew = countOfCrew;
+            this.codeOfPlane = codeOfPlane;
+        }
+
+        public Airplane(double distanceOfFlight, int countOfEngines, double volumeOfFuel)
+        {
+            this.distanceOfFlight = distanceOfFlight;
+            this.countOfEngines = countOfEngines;
+            this.volumeOfFuel = volumeOfFuel;
+        }
+
+        // 7.Создать статический конструктор.
+        static Airplane()
+        {
+            
+        }
+
+        // 3.Создать не менее трех методов управления классом и методы доступа к его закрытым полям.
+        public void SetNameOfCompany(string nameOfCompany)
+        {
+            this.nameOfCompany = nameOfCompany; 
+        }
+        public string GetNameOfCompany()
+        {
+            return nameOfCompany;
+        }
+        public void SetCodeOfPlane(string codeOfPlane)
+        {
+            this.codeOfPlane = codeOfPlane;
+        }
+        public string GetCodeOfPlane()
+        {
+            return codeOfPlane;
+        }
+        public void SetCountOFPassangers(int countOfPassangers)
+        {
+            this.countOfPassangers = countOfPassangers;
+        }
+        public int GetCountOFPassangers()
+        {
+            return countOfPassangers;
+        }
+        public void SetCountOfCrew(int countOfCrew)
+        {
+            this.countOfCrew = countOfCrew;
+        }
+        public int GetCountOfCrew()
+        {
+            return countOfCrew;
+        }
+        public void SetDistanceOfFlight(double distanceOfFlight)
+        {
+            this.distanceOfFlight = distanceOfFlight;
+        }
+        public double GetDistanceOfFlight()
+        {
+            return distanceOfFlight;
+        }
+        public void SetCountOfEngines(int countOfEngines)
+        {
+            this.countOfEngines = countOfEngines;
+        }
+        public int GetCountOfEngines()
+        {
+            return countOfEngines;
+        }
+        public void SetVolumeOfFuel(double volumeOfFuel)
+        {
+            this.volumeOfFuel = volumeOfFuel;
+        }
+        public double GetVolumeOfFuel()
+        {
+            return volumeOfFuel;
+        }
+
+        public void RefuelPlane(double newFuel)
+        {
+            volumeOfFuel += newFuel;
+        }
+
+        public void InsertPassangers(int newPass)
+        {
+            countOfPassangers += newPass;
+        }
+
+        public void RemovePassangers(int badPass)
+        {
+            countOfPassangers -= badPass;
+        }
+
+       
+        
+
+    }
+}
---
+++ ClassLessonHomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLessonHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 8.Создать массив(не менее 5 элементов) объектов созданного класса.
+            Airplane[] planes = { new Airplane(180,10,3000), new Airplane(240,8,6000), new Airplane(180,10,"KC275"), new Airplane(2450.5,4,600), new Airplane(5,2,150) };
+
+        }
+    }
+}
+
+// 9.Создать дополнительный метод для данного класса в другом файле, используя ключевое слово partial.
+public partial class Airplane
+{
+    public void Flight()
+    {
+        Console.WriteLine("Airplane in flight");
+    }
+
+}
